eae871011c119203d135da908e82140c1153d8b9
miinimanimo <ssk07079@gmail.com>
2024-11-16 14:33:10 +0900

    git 꼬임 정리 완료....

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.moodly"
         minSdk = 30
-        targetSdk = 35
+//        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -19,7 +19,6 @@ android {
 
     buildFeatures {
         viewBinding = true  // View Binding 활성화
-        // compose = true   // Compose 설정 제거
     }
 
     buildTypes {
@@ -50,6 +49,8 @@ dependencies {
     // 네비게이션 라이브러리
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.1")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.1")
+    implementation("com.google.android.material:material:1.8.0")
+
 
 
     // 테스트 라이브러리
--- app/src/main/java/com/example/moodly/MainActivity.kt
+++ app/src/main/java/com/example/moodly/MainActivity.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.moodly.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
-        bottomNavigationView.setupWithNavController(navController)
+        // 뷰 바인딩을 통해 bottomNavigationView 참조
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
